984b67ba6229d144e6d919ce8e755575d8bb280b
S <s>
2013-05-24 16:56:14 -0700

    {"type":"TI", "runs":"1"}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -55,10 +55,11 @@ public class NameSurferGraph extends GCanvas
 		int lineSpacing = (getWidth()-(2*GRAPH_MARGIN_SIZE)) / NDECADES;
 		for (int i=0 ; i <= NDECADES ; i++) {
 			drawLine(GRAPH_MARGIN_SIZE+(i*lineSpacing));
+			drawLabel(GRAPH_MARGIN_SIZE+(i*lineSpacing) , i);
 		}
 		drawHorizontalLine(GRAPH_MARGIN_SIZE);
 	}
-	private void drawLine(double x) {
+	private void drawLine(int x) {
 		GLine line = new GLine(x, GRAPH_MARGIN_SIZE , x , getHeight() - GRAPH_MARGIN_SIZE);
 		add(line);
 	}
@@ -67,6 +68,25 @@ public class NameSurferGraph extends GCanvas
 		GLine line2 = new GLine (x, getHeight() - x , getWidth() - x , getHeight() - x);
 		add(line);
 		add(line2);
+	}
+	private void drawLabel(int x , int i) {
+		int date = 0;
+		switch (i){
+		case 0: date = 1900;
+		case 1: date = 1910;
+		case 2: date = 1920;
+		case 3: date = 1930;
+		case 4: date = 1940;
+		case 5: date = 1950;
+		case 6: date = 1960;
+		case 7: date = 1970;
+		case 8: date = 1980;
+		case 9: date = 1990;
+		case 10: date = 2000;
+		case 11: date = 2010;
+		}
+		GLabel label = new GLabel(Integer.toString(date), x , getHeight()-GRAPH_MARGIN_SIZE);
+		add(label);
 	}
 	
 	
