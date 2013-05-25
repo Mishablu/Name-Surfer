0589e64b21c629e32efdd2c8c6026c627ccbb2cb
S <s>
2013-05-24 22:32:26 -0700

    {"type":"TI", "runs":"1"}

--- NameSurfer.java
+++ NameSurfer.java
@@ -54,13 +54,11 @@ public class NameSurfer extends Program implements NameSurferConstants {
 		
 		if(e.getActionCommand().equals("Clear")){
 			graph.clear();
-			graph.update();
 		} else if (e.getActionCommand().equals("Graph") || e.getSource() == nameField) {
 			String str = formatName(nameField.getText());
 			NameSurferEntry entry = dataBase.findEntry(str);
 			if (entry != null) {
 				graph.addEntry(entry);
-				graph.update();
 			}
 		} else if (e.getActionCommand().equals("Delete")){
 			String str = formatName(nameField.getText());
