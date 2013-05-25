87a74dbc02f18fa12fec7d7ef647ec968bfa4409
S <s>
2013-05-24 17:58:28 -0700

    {"type":"TI", "runs":"1"}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -61,12 +61,12 @@ public class NameSurferGraph extends GCanvas
 	public void update() {
 		//check that this remove all doesnt cause bugzzz
 		removeAll();		
-		int lineSpacing = (getWidth()-(2*GRAPH_MARGIN_SIZE)) / (NDECADES-1);
-		drawLinesAndLabels(lineSpacing);
-		//add horizontal lines
+		int lineSpacing = (getWidth()-(2*GRAPH_MARGIN_SIZE)) / (NDECADES);
+		drawGrid(lineSpacing);
+		
 	}
-	public void drawLinesAndLabels(int lineSpacing) {
-		for (int i=0 ; i < NDECADES ; i++) {
+	public void drawGrid(int lineSpacing) {
+		for (int i=0 ; i <= NDECADES ; i++) {
 			double x = GRAPH_MARGIN_SIZE + (i * lineSpacing);
 			add(new GLine(x, GRAPH_MARGIN_SIZE, x, getHeight() - GRAPH_MARGIN_SIZE));
 			GLabel label = new GLabel(Integer.toString(START_DECADE+(i*10)), x , getHeight()-(GRAPH_MARGIN_SIZE/3));
